ef63b41035723d424901fb82fac2380dc92d6079
Madhavan <nkmadhavan@yahoo.com>
2021-12-26 19:47:27 -0600

    Fixed the db import during app run

--- app.py
+++ app.py
@@ -11,10 +11,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "Not so secret"
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
     
 jwt = JWT(app, authenticate, identity)
 
